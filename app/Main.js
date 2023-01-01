ffcbb4cb7b54b010c8b0d48370f93f5d2f69637f
jack <Ciao@lazych.me>
2023-01-01 17:11:06 +0100

    immer

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useState } from "react"
+import { useImmerReducer } from "use-immer"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Axios from "axios"
@@ -25,18 +26,21 @@ function Main() {
     flashMessages: [],
   }
 
-  function ourReducer(state, action) {
+  function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        return { loggedIn: true, flashMessages: state.flashMessages }
+        draft.loggedIn = true
+        return
       case "logout":
-        return { loggedIn: false, flashMessages: state.flashMessages }
+        draft.loggedIn = false
+        return
       case "flashMessage":
-        return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+        draft.flashMessages.push(action.value)
+        return
     }
   }
 
-  const [state, dispatch] = useReducer(ourReducer, initialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
   return (
     <StateContext.Provider value={state}>
