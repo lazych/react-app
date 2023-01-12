96b5a1f35a60ca0f3c18bc8c737806fd93a4b1e3
jack <Ciao@lazych.me>
2023-01-12 16:22:22 +0100

    i added chat feature

--- app/Main.js
+++ app/Main.js
@@ -24,6 +24,7 @@ import Profile from "./components/Profile"
 import EditPost from "./components/EditPost"
 import NotFound from "./components/NotFound"
 import Search from "./components/Search"
+import Chat from "./components/Chat"
 
 function Main() {
   const initialState = {
@@ -35,6 +36,8 @@ function Main() {
       avatar: localStorage.getItem("appAvatar"),
     },
     isSearchOpen: false,
+    isChatOpen: false,
+    unreadChatCount: 0,
   }
 
   function ourReducer(draft, action) {
@@ -54,6 +57,18 @@ function Main() {
         return
       case "closeSearch":
         draft.isSearchOpen = false
+        return
+      case "toggleChat":
+        draft.isChatOpen = !draft.isChatOpen
+        return
+      case "closeChat":
+        draft.isChatOpen = false
+        return
+      case "incrementUnreadChatCount":
+        draft.unreadChatCount++
+        return
+      case "clearUnreadChatCount":
+        draft.unreadChatCount = 0
         return
     }
   }
@@ -92,6 +107,7 @@ function Main() {
             <Search />
           </CSSTransition>
           <Footer />
+          <Chat />
         </BrowserRouter>
       </DispatchContext.Provider>
     </StateContext.Provider>
